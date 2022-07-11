1984c8e18ddfc2bf78d9240f1e670c0c7da34480
Anthony Ozouli <105608183+flawlez18@users.noreply.github.com>
2022-07-11 08:58:52 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http://http://18.208.223.255:9000/',
+  serverUrl: 'http://18.208.223.255:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
